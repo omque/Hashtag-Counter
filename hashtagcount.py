3662ff3b27598012b0b166658fc22763043dbabc
omque <omqazi@gmail.com>
2018-04-08 21:23:40 +0900

    Cleaned up language to make variables clearer

--- hashtagcount.py
+++ hashtagcount.py
@@ -1,5 +1,7 @@
-message = "#japan #japanese, #test"
+#! python3
+# hashtagcount.py - Quickly check how many hashtags you have before posting to Instagram.
 
-print("The hastags are:", message)
-print("You have {} hashtags.".format(message.count("#")))
+dictionary = "#japan #japanese #japantravel #traveller #traveling #travelblog #japantrip #asiatravel#asiatrip #japanstyle #travelblogger #travelphotography #photography #photographer #travelasia #visitjapan #wanderlust #scoville #spicychicken #latenighteats #peppers #ghostpeppers #lastdab #hotones #chilies #seanevans #chickenwings #peppers #hotsauce #spicy #youarewhatyoueat #japan #japanese, #test"
 
+print("The hashtags are:", dictionary)
+print("You have {} hashtags.".format(dictionary.count("#")))
