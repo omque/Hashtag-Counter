3639cd7dd2d043fb6147db2f2be1368c3d14da87
omque <omqazi@gmail.com>
2018-04-09 00:01:38 +0900

    Closed Sublime

--- hashtagcount.py
+++ hashtagcount.py
@@ -1,7 +1,15 @@
 #! python3
 # hashtagcount.py - Quickly check how many hashtags you have before posting to Instagram.
 
-dictionary = "#japan #japanese #japantravel #traveller #traveling #travelblog #japantrip #asiatravel#asiatrip #japanstyle #travelblogger #travelphotography #photography #photographer #travelasia #visitjapan #wanderlust #scoville #spicychicken #latenighteats #peppers #ghostpeppers #lastdab #hotones #chilies #seanevans #chickenwings #peppers #hotsauce #spicy #youarewhatyoueat #japan #japanese, #test"
+import sys
 
-print("The hashtags are:", dictionary)
-print("You have {} hashtags.".format(dictionary.count("#")))
+if len(sys.argv) < 2:
+	print("Please enter some hashtags to count as a command line argument.")
+	sys.exit()
+
+#dictionary = "#japan #japanese #japantravel #traveller #traveling #travelblog #japantrip #asiatravel#asiatrip #japanstyle #travelblogger #travelphotography #photography #photographer #travelasia #visitjapan #wanderlust #scoville #spicychicken #latenighteats #peppers #ghostpeppers #lastdab #hotones #chilies #seanevans #chickenwings #peppers #hotsauce #spicy #youarewhatyoueat #japan #japanese, #test"
+
+
+
+#print("The hashtags are:", dictionary)
+#print("You have {} hashtags.".format(dictionary.count("#")))
